6f3b958f846dd8f13f34c3251730b11550f531d8
Alle Madhu <madhu17apr@gmail.com>
2023-02-02 18:29:36 +0530

    Removed direct calling the service in ServiceToGetTimezoneCurrentTime class and passed core config.factory object in services/yml file

--- src/Service/ServiceToGetTimezoneCurrentTime.php
+++ src/Service/ServiceToGetTimezoneCurrentTime.php
@@ -4,22 +4,41 @@ namespace Drupal\site_location_and_time\Service;
 
 use DateTime;
 use DateTimeZone;
-use Drupal;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class GetTimezoneCurrentTime.
  */
-class ServiceToGetTimezoneCurrentTime {
+class ServiceToGetTimezoneCurrentTime  {
 
+  /**
+   * Summary of config_factory
+   * @var mixed
+   */
+  protected $config_factory;
+
+  /**
+   * Summary of __construct
+   * @param ContainerInterface $config_factory
+   */
+  public function __construct($config_factory) {
+    $this->config_factory = $config_factory;
+  }
+
+  /**
+   * Summary of getTimezoneCurrentTime
+   * @return string
+   */
   function getTimezoneCurrentTime(){
 
-    $config = Drupal::config('site_location_and_time.locationandtimezone');
+    $config = $this->config_factory->get('site_location_and_time.locationandtimezone');
+
     $timezone = $config->get('timezone');
     $datetime = new DateTime('now', new DateTimeZone($timezone));
     $current_time = $datetime->format('d M Y - H:i a');
-
     return $current_time;
 
   }
+
 
 }
